94c5e02ce3b8ec7f8e3e8adfc634dc954605bfbe
Justell <juste.valentelyte@gmail.com>
2022-11-02 21:21:25 +0200

    feat(app): improve layout and home views

--- star-wars/src/app/home/home.component.ts
+++ star-wars/src/app/home/home.component.ts
@@ -1,16 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+  constructor() {}
 }
