0c2c49d03df80820b8d61d4d0ba919ebc6828043
Justell <juste.valentelyte@gmail.com>
2022-11-03 11:08:27 +0200

    feat(movies): update guard and detail view

--- star-wars/src/app/movies/guards/movie-exist.guard.ts
+++ star-wars/src/app/movies/guards/movie-exist.guard.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { catchError, Observable, of, switchMap } from 'rxjs';
 
-import { MovieDetailActions, MovieDetailsSelectors, State } from '../store';
+import { MovieDetailsSelectors, State } from '../store';
 
 @Injectable()
 export class MovieExistGuard implements CanActivate {
-  constructor(private store: Store<State>) {}
+  constructor(private store: Store<State>, private router: Router) {}
 
   canActivate(): Observable<boolean> {
     return this.store.select(MovieDetailsSelectors.getMovie).pipe(
       switchMap((movie) => {
         if (!movie) {
-          this.store.dispatch(MovieDetailActions.loadMovie({ id: '1' }));
+          this.router.navigate(['/home']);
         }
 
         return of(true);
